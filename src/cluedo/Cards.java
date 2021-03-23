8315309014db904cd30becd7b9cb92f762d78f4c
matthewli1013 <80222855+matthewli1013@users.noreply.github.com>
2021-03-23 22:56:50 +0000

    Add files via upload
    added class Cards

---
+++ src/cluedo/Cards.java
@@ -0,0 +1,47 @@
+package cluedo;
+
+/**
+ *
+ * @author matth
+ */
+import java.util.Queue;
+import java.util.ArrayList;
+
+public class Cards {
+    public class playerCards {
+        String scarlett = "Miss Scarlett";
+        String green = "Rev Green";
+        String mustard = "Colonel Mustard";
+        String plum = "Professor Plum";
+        String peacock = "Mrs. Peacock";
+        String white  = "Mr. White";
+        
+    }
+    public class roomCards {
+        String kitchen = "Kitchen";
+        String ballroom = "Ballroom";
+        String conservatory = "Conservatory";
+        String dinning = "Dinning Room";
+        String billard = "Billard Room";
+        String library = "Library";
+        String lounge = "Lounge";
+        String hall = "Hall";
+        String strudy = "Study";
+        
+    }
+    public class weaponCards {
+        String candlestick = "Candlestick";
+        String dagger = "Dagger";
+        String pipe = "Lead Pipe";
+        String revolver = "Revolver";
+        String rope = "Rope";
+        String wrench = "Wrench";
+        
+    }
+    
+    public class envelopeCards {
+        
+}
+    
+    
+}
