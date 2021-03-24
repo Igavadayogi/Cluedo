9c101514060a972f4744890fbf429a3c265d7fb4
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-03-24 00:18:12 +0000

    Ai player class added

---
+++ src/cluedo/AIPlayer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo;
+
+/**
+ *
+ * @author user
+ */
+public class AIPlayer {
+    
+    //an array filled with actions
+    //e.g.: [move,move,move,accuse,move,move,move]
+    //use RNG to pick one action
+    
+}
