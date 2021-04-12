58f2e124a0209f85ad1457b7a15944339fe4bd79
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-04-12 22:07:12 +0100

    dice class fixed, auto-roll dice added in text based version

--- src/cluedo/GameBoard.java
+++ src/cluedo/GameBoard.java
@@ -21,126 +21,138 @@ import javax.swing.border.LineBorder;
  *
  * @author user
  */
-public class GameBoard implements ActionListener{
-    
-    String [][] boardArray = new String [24][25];
+public class GameBoard implements ActionListener {
+
+    String[][] boardArray = new String[24][25];
     private int input1;
     private int input2;
-   
-    
-    GameBoard(){
+
+    GameBoard() {
+        
         createBoard();
         print();
-        playerInput();
-        movePiece();
-        print();
+        run();
     }
-    
-    public final void createBoard(){
-      //i is y
-      //j is x
-        for(int i = 0; i< 24; i++){
-            for(int j = 0; j< 25; j++){
+
+    public void run() {
+       //Dice dice = new Dice();
+        
+        while (true) {
+            rollDice();
+            playerInput();
+            movePiece();
+            print();
+        }
+    }
+
+    public final void createBoard() {
+        //i is y
+        //j is x
+        System.out.println("  0    1    2    3    4    5    6    7    8    9    10   11   12   13   14    15   16   17   18   19   20  21  22  23  24   25");//temp for testing
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 25; j++) {
                 boardArray[i][j] = " _ ";
             }
         }
-        
+
         boardArray[12][9] = " A ";
         boardArray[13][9] = " B ";
         boardArray[14][9] = " C ";
         boardArray[12][13] = " D ";
         boardArray[13][13] = " E ";
         boardArray[14][13] = " F ";
-        
+
 //        for(int i = 0; i<6; i++){
 //            for(int j = 0; i<4; j++){
 //                boardArray[i][j] = "Study";
 //            }
 //        }
-    
     }
-    
-       public final void print(){
-            //temp 
-            for (int i = 0; i <24; i++) {
-			for (int j = 0; j < 25; j++) {
-				System.out.print(" " + boardArray[i][j] + " ");
-			}
-			System.out.println();
 
-		}
+    public final void print() {
+        //temp 
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 25; j++) {
+                System.out.print(" " + boardArray[i][j] + " ");
+            }
+            System.out.println();
+
         }
-       
-    public void movePiece(){
+    }
+
+    public void movePiece() {
         //temp method
         int a = input1;
         int b = input2;
-        
+
         boardArray[input2][input1] = " A ";
-        
+
     }
-    
-    public void playerInput(){
+
+    public void playerInput() {
         Scanner s = new Scanner(System.in);
         input1 = s.nextInt(); //inputs for y-x coordinates 
-	input2 = s.nextInt();
-        
+        input2 = s.nextInt();
+
     }
-    public void accuse(){
+
+    public void accuse() {
         //make accusations
         //player will need to choose the cards either before or at this step
         //show a frame with 3 lists, after choosing, then press enter
     }
-    
-    public void checkAccusation(){
+
+    public void checkAccusation() {
         //check if the player's accusation matches with the envelope
         //get contents of the envelope and compare
         //if true that player wins
-      
+
         //get from envelope
         //if accuse cards = envelope card then call win emthod
         //otheriwse game continues
     }
-    
-    public void win(){
+
+    public void win() {
         //display win message, end game
     }
 
-    public void movePlayer(){
+    public void movePlayer() {
         //click on board, if exceed diceroll display error message
         //if diagonal display error
-        
+
         //get the x-y coordinates from player input method
     }
-    
-    public void checkMove(){
+
+    public void checkMove() {
         //check with dice and if diagnal(compare with board coordinates
         //diplay error when player move into room without using the door  
     }
-    
-    public void nextTurn(){
+
+    public void nextTurn() {
         //give control to next player
         //show next player information, store and remove current player information
     }
-    
-    public void rollDice(){
+
+    public static void rollDice() {
         //done, just call from Dice class
         //when the button is pressed, this method will be called.
+       Dice dice = new Dice();
+       System.out.println("Dice result:" + dice.getDice2() + "," + dice.getDice1());
     }
-    
-    public void chooseSaveGameOrNot(){
+
+    public void chooseSaveGameOrNot() {
         //shows when exiting the game
     }
-    
-    public void saveGame(){
+
+    public void saveGame() {
         //write text file, use buffer reader
         //should be a button 
     }
-    
-    public void loadGame(){
+
+    public void loadGame() {
         //load from text file, use file reader
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
