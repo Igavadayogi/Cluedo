d100a10ea31b5e449a4a30a16918999c322cbee0
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-04-12 22:08:39 +0100

    dice class fixed, auto-roll dice added in text based version

--- src/cluedo/Dice.java
+++ src/cluedo/Dice.java
@@ -8,18 +8,20 @@ package cluedo;
  * class for dice
  * @author Sze Yuen Kwok
  */
+
 public class Dice { 
-  int dice1 = (int)(Math.random() * (7-1) + 1);
-  int dice2 = (int)(Math.random() * (7-1) + 1);
+int dice1;
+int dice2;
   
-  public Dice(int dice1, int dice2){
-      this.dice1 = dice1;
-      this.dice2 = dice2;
+  public Dice(){
+        dice1 = (int)(Math.random() * (7-1) + 1);
+        dice2 = (int)(Math.random() * (7-1) + 1);
   }
-  public int getDice1(){
+
+  public  int getDice1(){
       return dice1;
   }
-  public int getDice2(){
+  public  int getDice2(){
       return dice2;
   }
 }
