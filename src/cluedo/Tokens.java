6590e6c41c51ca743e6dcfaa7635d55b03e002ea
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-04-24 15:16:36 +0100

    added classes

---
+++ src/cluedo/Tokens.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo;
+
+/**
+ * This class is for the actual pieces on the board, not to be confused with players
+ * @author user
+ */
+
+
+public class Tokens {
+    
+}
