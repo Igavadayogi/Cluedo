f995c8aeb7c58f53bd5d367a98cb22a31205fa44
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-04-25 16:05:33 +0100

    text based menu added

--- src/cluedo/Cluedo.java
+++ src/cluedo/Cluedo.java
@@ -5,6 +5,8 @@
  */
 package cluedo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author igava
@@ -15,9 +17,28 @@ public class Cluedo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//         Menu menu = new Menu();
-//         menu.setVisible(true);
-          Game game = new Game();
+
+        System.out.println("Welcome to Cluedo!");
+        System.out.println("Press 1 to Start");
+        System.out.println("Press 0 to Exit");
+        System.out.println("");
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        switch (input) {
+            case "1":
+                Game game = new Game();
+                break;
+            case "0":
+                System.out.println("Goodbye");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Command not recognised");
+                break;
+        }
+
     }
-    
+
 }
