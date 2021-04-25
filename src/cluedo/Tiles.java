8221c2352d7de57c9230f54ae8b41a3da63abe37
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-04-25 19:59:54 +0100

    tile updated

--- src/cluedo/Tiles.java
+++ src/cluedo/Tiles.java
@@ -17,45 +17,28 @@ public class Tiles {
     private int y;
 
     public Tiles(int x, int y, Tokens token) {
-        this.setXPos(x);
-        this.setYPos(y);
-        this.setToken(token);
+        this.x = x;
+        this.y = y;
+        this.token = token;
     }
-    
-    
-    //may be not needed, incomplete
-    public boolean isWall(){
-        return false;
-    }
-    
+
     //incomplete
-    public boolean occupied(Tokens token){
+    public boolean occupied(Tokens token) {
         //if token is already on tile, this tile is not available
+        //default is false;
         return false;
     }
 
     public int getXPos() {
         return this.x;
     }
-    
-    public void setXPos(int x) {
-        this.x = x;
-    }
 
     public int getYPos() {
         return this.y;
     }
-    
-     public void setYPos(int y) {
-        this.y = y;
-    }
 
     public Tokens getToken() {
         return this.token;
-    }
-
-    public void setToken(Tokens tkn) {
-        this.token = tkn;
     }
 
 }
