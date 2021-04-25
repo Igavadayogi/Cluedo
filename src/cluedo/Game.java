4ebb1c5ef84f7ec05484490d8f3943b8863283ce
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2021-04-25 18:42:22 +0100

    minor stuff

--- src/cluedo/Game.java
+++ src/cluedo/Game.java
@@ -18,98 +18,14 @@ import java.util.Scanner;
 public class Game implements ActionListener {
     
     private Player[] player;
-    private Board board;
+    public Board board;
     private Player presentTurn;
 
-    
-//    String[][] boardArray = new String[24][25];
-//    private int input1;
-//    private int input2;
-    
-//    Player player1 = new Player("Miss Scarlett");
-//    Player player2 = new Player("Rev Green");
-//    Player player3 = new Player("Colonel Mustard");
-//    Player player4 = new Player("Professor Plum");
-//    Player player5 = new Player("Mrs. Peacock");
-//    Player player6 = new Player("Mr. White");
-
-//    Game() {
-//        createBoard();
-//        print();
-//        run();
-//    }
     private void init(){
         
     } 
     
-    
 
-//    public void run() { //this part is temporary
-//
-//        while (true) {
-//            rollDice();
-//            playerInput();
-//            movePiece();
-//            print();
-//        }
-//    }
-
-    //will move these to the new board class
-//    public final void createBoard() {
-//        //i is y
-//        //j is x
-//        System.out.println("  0    1    2    3    4    5    6    7    8    9    10   11   12   13   14    15   16   17   18   19   20  21  22  23  24   25");//temp for testing
-//        for (int i = 0; i < 24; i++) {
-//            for (int j = 0; j < 25; j++) {
-//                boardArray[i][j] = " _ ";
-//            }
-//        }
-//
-//        boardArray[12][9] = " A ";
-//        boardArray[13][9] = " B ";
-//        boardArray[14][9] = " C ";
-//        boardArray[12][13] = " D ";
-//        boardArray[13][13] = " E ";
-//        boardArray[14][13] = " F ";
-//
-////        for(int i = 0; i<6; i++){
-////            for(int j = 0; i<4; j++){
-////                boardArray[i][j] = "Study";
-////            }
-////        }
-//    }
-
-//    public final void print() {
-//        //temp 
-//        for (int i = 0; i < 24; i++) {
-//            for (int j = 0; j < 25; j++) {
-//                System.out.print(" " + boardArray[i][j] + " ");
-//            }
-//            System.out.println();
-//
-//        }
-//    }
-    
-//  something like this to get coordinate of players
-//    public void showPlayerLocation(playerNumber){
-//        playerNumber.getXCoord(); 
-//    }
-    
-//    public void movePiece() {
-//        //temp method
-//        int a = input1;
-//        int b = input2;
-//
-//        boardArray[input2][input1] = " A ";
-//
-//    }
-
-//    public void playerInput() {
-//        Scanner s = new Scanner(System.in);
-//        input1 = s.nextInt(); //inputs for y-x coordinates 
-//        input2 = s.nextInt();
-//
-//    }
 
     public void accuse() {
         //make accusations
@@ -168,55 +84,67 @@ public class Game implements ActionListener {
         //load from text file, use file reader
     }
 
-    public void commenceMove(Player user1){
+    public void commenceMove(Player player1, Player player2, Player player3, Player player4, Player player5, Player player6){
         
-        //Move the player
-        player[1] = user1;
+        
+        player[0] = player1;
+        player[1] = player2;
+        player[2] = player3;
+        player[3] = player4;
+        player[4] = player5;
+        player[5] = player6;
+        
         
         board.restoreBoard();
+        
+        //temp for test
+        this.presentTurn = player1;
         
         /*
         if(user1.isMsScarlett()){
             this.presentTurn = user1;
         }
         */
-        
-        public boolean playerAction(Player player, int beginX, int beginY, int finishX, int finish Y){
-        
-            Tiles beginBA = board.getboardArray(beginX, finishY);
-            Tiles finishBA = board.getboardArray(beginY, finishY);
-            Movement move = new Movement(player, beginBA, finishBA);
-            //return this.createMove(move, player);
-        }
-        
-        public boolean createMove(Movement move, Player player){
-            
-            Tokens rootTokens = move.getBegin().getTokens();
-            if(rootTokens == null){
-                return false;
-            }
-            
-            if(player != presentTurn){
-                return false;
-            }
-            
-            //commented because Ms Scarlett is not defined yet
-            /*
-            if(rootTokens.isMsScralett() != player.isMsScarlett){
-                return false;
-            }
-            */
-            
-            //commented because canMove isn't implemented
-            //Or it's been implemented but idk 
-            /*
-            if(!rootTokens.canMove(board, move.getBegin(), move.getFinish)){
-                return false
-            }
-            */
-        }
-        
     }
+    
+        //move by replacing
+//        public boolean playerAction(Player player, int beginX, int beginY, int finishX, int finishY) throws Exception{
+//        
+//            Tiles beginBA = board.getboardArray(beginX, finishY);
+//            Tiles finishBA = board.getboardArray(beginY, finishY);
+//            Movement move = new Movement(player, beginBA, finishBA);
+//            return this.createMove(move, player);
+//        
+//        }
+//        
+//        public boolean createMove(Movement move, Player player){
+//            
+//            Tokens rootTokens = move.getStart().getTokens();
+//            if(rootTokens == null){
+//                return false;
+//            }
+//            
+//            if(player != presentTurn){
+//                return false;
+//            }
+//            
+//            //commented because Ms Scarlett is not defined yet
+//            /*
+//            if(rootTokens.isMsScralett() != player.isMsScarlett){
+//                return false;
+//            }
+//            */
+//            
+//            //commented because canMove isn't implemented
+//            //Or it's been implemented but idk 
+//            /*
+//            if(!rootTokens.canMove(board, move.getBegin(), move.getFinish)){
+//                return false
+//            }
+//            */
+//        }
+        
+    
            
     @Override
     public void actionPerformed(ActionEvent ae) {
