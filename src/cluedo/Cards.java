036973b8672dc05c5f24976e8bd6b4fdfc727227
user <user@MSI>
2021-04-26 18:36:09 +0100

    Added three arraylist for all categories of cards

--- src/cluedo/Cards.java
+++ src/cluedo/Cards.java
@@ -1,5 +1,8 @@
 package cluedo;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 /**
  *
  * @author matth
@@ -7,50 +10,50 @@ package cluedo;
 
 public class Cards {
     //public class characterCards {
-        String scarlett = "Miss Scarlett";
-        String green = "Rev Green";
-        String mustard = "Colonel Mustard";
-        String plum = "Professor Plum";
-        String peacock = "Mrs. Peacock";
-        String white  = "Mr. White";
+    ArrayList<String> characterCards = new ArrayList<>(Arrays.asList("Ms Scarlett", "Rev Green", "Colonel Mustard", "Mrs. Peacock", "Mr. White"));
+        //String green = "Rev Green";
+        //String mustard = "Colonel Mustard";
+        //String plum = "Professor Plum";
+        //String peacock = "Mrs. Peacock";
+        //String white  = "Mr. White";
         
-         String[] character = new String[] {"Miss Scarlett", "Rev Green", "Colonel Mustard",
-        "Professor Plum", "Mrs. Peacock", "Mr. White"};
+         //ArrayList<String> character = new String[] {"Miss Scarlett", "Rev Green", "Colonel Mustard",
+        //"Professor Plum", "Mrs. Peacock", "Mr. White"};
     //}
    // public class roomCards {
-        String kitchen = "Kitchen";
-        String ballroom = "Ballroom";
-        String conservatory = "Conservatory";
-        String dinning = "Dinning Room";
-        String billard = "Billard Room";
-        String library = "Library";
-        String lounge = "Lounge";
-        String hall = "Hall";
-        String study = "Study";
+    ArrayList<String> roomCards = new ArrayList<>(Arrays.asList("Kitchen", "Ballroom", "Conservatory", "Dinning Room", "Billard Room", "Library", "Lounge", "Hall", "Study"));
+        //String kitchen = "Kitchen";
+        //String ballroom = "Ballroom";
+        //String conservatory = "Conservatory";
+        //String dinning = "Dinning Room";
+        //String billard = "Billard Room";
+        //String library = "Library";
+        //String lounge = "Lounge";
+        //String hall = "Hall";
+        //String study = "Study";
         
-  String rooms[] = new String[] {"Kitchen","Ballroom","Conservatory", "Dinning Room","Billard Room",
-        "Library","Lounge", "Hall", "Study"};
+  //String rooms[] = new String[] {"Kitchen","Ballroom","Conservatory", "Dinning Room","Billard Room",
+        //"Library","Lounge", "Hall", "Study"};
     //}
     //public class weaponCards {
-        String candlestick = "Candlestick";
-        String dagger = "Dagger";
-        String pipe = "Lead Pipe";
-        String revolver = "Revolver";
-        String rope = "Rope";
-        String wrench = "Wrench";
+    ArrayList<String> weaponCards = new ArrayList<>(Arrays.asList("Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench"));
+        //String candlestick = "Candlestick";
+        //String dagger = "Dagger";
+        //String pipe = "Lead Pipe";
+        //String revolver = "Revolver";
+        //String rope = "Rope";
+        //String wrench = "Wrench";
      
-        String weapons[] =  new String[] {"Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench"};
+        //String weapons[] =  new String[] {"Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench"};
     //}
     
     //public class envelopeCards {
 
-        //choose 1 randomly from each class, fo a total of 3 cards,
-        //put in array/assign numbe then use random number generator to determine
-       
-        String chosenCharacter = character[(int)(Math.random() * (5-0) + 0)];
-        String chosenRoom = rooms[(int)(Math.random() * (8-0) + 0)];
-        String chosenWeapon = weapons[(int)(Math.random() * (5-0) + 0)];
-    //}
+    //choose 1 randomly from each class, fo a total of 3 cards,
+    Random random = new Random();
+    final String chosenCharacter = characterCards.get(random.nextInt(characterCards.size()));
+    final String chosenRoom = roomCards.get(random.nextInt(roomCards.size()));
+    final String chosenWeapon = weaponCards.get(random.nextInt(weaponCards.size()));
     
      public String getCharacter(){
          return chosenCharacter;
@@ -62,4 +65,7 @@ public class Cards {
          return chosenWeapon;
      }
     
+     public void getStartingCards(){
+         
+     }
 }
